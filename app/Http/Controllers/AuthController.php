500bb88664ca160c631ee1882b8aa76d84622e4d
kinggiddy1 <kinggiddy@gmail.com>
2025-01-15 16:56:52 +0200

    auth completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,11 +24,43 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => $request->role ?? 'User', 
+            'role' => $request->role ?? 'Admin', 
         ]);
 
         $token = JWTAuth::fromUser($user);
 
         return response()->json(['user' => $user, 'token' => $token]);
     }
+
+     // Login
+     public function login(Request $request)
+     {
+         $validator = Validator::make($request->all(), [
+             'email' => 'required|email|exists:users,email',
+             'password' => 'required'
+         ]);
+ 
+         if ($validator->fails()) {
+             return response()->json(['errors' => $validator->errors()], 422);
+         }
+ 
+         $credentials = $request->only('email', 'password');
+ 
+         if (!$token = JWTAuth::attempt($credentials)) {
+             return response()->json([
+                 'errors' => [
+                   'password' =>['Provided password is incorrect.']
+                 ]
+             ], 401); 
+         }
+ 
+         return response()->json(['token' => $token]); 
+     }
+ 
+     // Logout
+     public function logout()
+     {
+         auth()->logout();
+         return response()->json(['message' => 'Successfully logged out']);
+     }
 }
